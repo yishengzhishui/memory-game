f62447a3479576b2bff1a37c9b58fe2d50a7b67b
wangxing <18912540921@163.com>
2018-03-03 20:38:53 +0800

    some trouble

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * 创建一个包含所有卡片的数组
  */
-let cardAll = ["fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor","fa fa-bolt","fa fa-cube","fa fa-leaf","fa fa-bicycle","fa fa-bomb","fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor","fa fa-bolt","fa fa-cube","fa fa-leaf","fa fa-bicycle","fa fa-bomb"];
+let cardAll = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb"];
 /*
  * 显示页面上的卡片
  *   - 使用下面提供的 "shuffle" 方法对数组中的卡片进行洗牌
@@ -11,17 +11,18 @@ let cardAll = ["fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor","fa fa-bolt"
 
 // 洗牌函数来自于 http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 };
 /*
  * 设置一张卡片的事件监听器。 如果该卡片被点击：
@@ -39,9 +40,9 @@ function shuffle(array) {
 function cardPosition(array) {
   $(".match").removeClass("match");
   $(".open.show").removeClass("open show");
-  for(let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     $(".deck").find("li").eq(i).children().attr("class", array[i])
- };
+  };
 };
 //卡片打开
 let showCard = function() {
@@ -56,49 +57,47 @@ let pushArray = function(array) {
   array.push($(event.target).children().attr("class"));
 };
 //判断card是否相同
-let judgeCard = function(openArray,matchArray) {
-  if (openArray.length === 2) {
-    if (openArray[0] === openArray[1]) {
-      $(".open.show").addClass("match");
-      closeCard();
-      matchArray.push(openArray[0]);
-      matchArray.push(openArray[1]);
-      openArray.length = 0;
-    } else {
-      $(".open.show").removeClass("disabled");//解除不准点击的锁定
-      closeCard();
-      openArray.length = 0;
-    };
+let judgeCard = function(openArray, matchArray) {
+  if (openArray[0] === openArray[1]) {
+    $(".open.show").addClass("match");
+    closeCard();
+    matchArray.push(openArray[0]);
+    matchArray.push(openArray[1]);
+  } else {
+    $(".open.show").removeClass("disabled"); //解除不准点击的锁定
+    setTimeout(closeCard, 1000);
   };
 };
 //点击次数和星星变化
 let starMoves = function(num) {
   $("span.moves").text(num);
-  if (num > 20 && num <=30) {
+  if (num > 20 && num <= 30) {
     $(".stars").find("li").eq(2).children().attr("class", "fa fa-star-o")
   } else if (num > 30) {
     $(".stars").find("li").eq(1).children().attr("class", "fa fa-star-o")
   }
 };
 //闯关结束判断
-let lastNew = function(array,num) {
+let lastNew = function(array, num) {
   let star = 1;
-  if (num <= 20 ) {
+  if (num <= 20) {
     star = 3;
-  } else if (num > 20 && num <=30) {
+  } else if (num > 20 && num <= 30) {
     star = 2;
   };
   if (array.length === 16) {
     timeOver();
     atime = $(".time").text();
-    alert(`恭喜过关共用${num}步，获得${star}颗星,用时${atime}秒`);
+    alert(`恭喜过关共用${num}步，获得${star}颗星,用时${atime}秒，再玩一次？`);
   }
 };
 //开始纪录时间
 let timeRecord = function(num) {
-  $(".time").text(num) ;
-  num = num + 1;
-  timef = setTimeout(function(){timeRecord(num)},1000);
+  $(".time").text(num);
+  num++;
+  timef = setTimeout(function() {
+    timeRecord(num)
+  }, 1000);
 };
 //暂停纪录时间
 let timeOver = function() {
@@ -113,7 +112,7 @@ let disabledCard = function() {
   $(event.target).addClass("disabled");
 }
 //重启步数
-let moveRestart = function(num,openArray,matchArray) {
+let moveRestart = function(num, openArray, matchArray) {
   openArray.length = 0;
   matchArray.length = 0;
   moveNum = 0;
@@ -131,7 +130,7 @@ let timef; //为了提高性能
 $(".restart").click(function() {
   let newArray = shuffle(cardAll);
   cardPosition(newArray);
-  moveRestart(moveNum,openCard,matchCard);
+  moveRestart(moveNum, openCard, matchCard);
   timeOver();
   timeRecord(timeNum);
 });
@@ -139,11 +138,17 @@ $(".restart").click(function() {
 $("li").click(function(event) {
   showCard();
   pushArray(openCard);
-  disabledCard();//这样写没有问题吗？
+  disabledCard(); //这样写没有问题吗？
   moveNum++;
   starMoves(moveNum);
-  disabledCards();
-  setTimeout(function(){judgeCard(openCard,matchCard)}, 800); //setTimeout写法
-  setTimeout(disabledCards,1200);
-  setTimeout(function(){lastNew(matchCard,moveNum,timeNum)},2000);
+  if (openCard.length === 2) {
+    disabledCards();
+    judgeCard(openCard, matchCard); //setTimeout写法s
+    openCard.length = 0;
+  };
+
+
+  setTimeout(function() {
+    lastNew(matchCard, moveNum, timeNum)
+  }, 2000);
 });
